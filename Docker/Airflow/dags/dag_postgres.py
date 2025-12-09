a30b07c4c78083967b37aa98ebf20453fdf6a53a
EFG <efg@efg.local>
2025-12-08 21:00:16 -0300

    Conclusão do Projeto

---
+++ Docker/Airflow/dags/dag_postgres.py
@@ -0,0 +1,67 @@
+# > Importações
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import numpy as np
+import random
+from airflow.providers.postgres.hooks.postgres import PostgresHook
+
+# > Argumentos
+# DAG
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 11, 25),
+    'retries': 1,
+    'catchup': False,
+    'retry_delay': timedelta(minutes=5),
+    'schedule_interval': '@daily'
+}
+with DAG(
+    'etl_postgres',
+    'etl_livros_ml',
+    default_args = default_args,
+    description = 'ETL de dados do Mercado Livre'
+) as dag:
+    
+    # > Operadores
+    # 1. Iniciar Pipeline
+    tarefa_iniciar_pipeline = PythonOperator(
+        task_id = 'tarefa_iniciar_pipeline',
+        python_callable = iniciar_pipeline,
+        dag = dag
+    )
+    # 2. Importar Dados dos Livros do CSV
+    tarefa_importar_dados_csv = PythonOperator(
+        task_id = 'tarefa_importar_dados_csv',
+        python_callable = obter_livros,
+    )
+    # 3. Criar Tabela para armazenar os livros
+    criar_tabela_livro = SQLExecuteQueryOperator(
+        task_id = 'criar_tabela_livro',
+        conn_id = 'postgres_connection',
+        sql = """
+        CREATE TABLE IF NOT EXISTS livro (
+            id SERIAL PRIMARY KEY,
+            isbn VARCHAR(20) UNIQUE NOT NULL,
+            titulo VARCHAR(255) NOT NULL,
+            autor VARCHAR(255) NOT NULL,
+            publicacao DATE,
+            editora VARCHAR(100),
+            imageurl VARCHAR(255),
+            preco NUMERIC(6, 2) NOT NULL
+        );
+        """
+    )
+    # 4. Inserir dados no banco
+    inserir_dados_livros = PythonOperator(
+        task_id='inserir_dados_livros',
+        python_callable = post.inserir_dados,
+    )
+
+    # > Ordem das Dependências
+    tarefa_iniciar_pipeline >> tarefa_importar_dados_csv >> criar_tabela_livro >> inserir_dados_livros
