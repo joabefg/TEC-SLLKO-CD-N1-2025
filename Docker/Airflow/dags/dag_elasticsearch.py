cf7115e93d84aee3ec571a31b5089c6611f5a713
EFG <efg@efg.local>
2025-12-08 21:00:28 -0300

    Conclusão do projeto

---
+++ Docker/Airflow/dags/dag_elasticsearch.py
@@ -0,0 +1,33 @@
+from datetime import datetime, timedelta
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+import funcoes_helper.funcoes_elasticsearch as funcoes 
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2025, 12, 3),
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
+    'schedule_interval': None
+}
+
+with DAG (
+    dag_id = 'etl_elasticsearch',
+    default_args = default_args,
+    catchup = False,
+) as dag:
+    # 1.Tarefa de Extração
+    tarefa_extracao =PythonOperator(
+        task_id = 'tarefa-extracao',
+        python_callable = funcoes.extrair_dados
+    )
+    # 2. Tarefa de Carregamento
+    tarefa_carregamento = PythonOperator(
+        task_id = 'tarefa_carregamento',
+        python_callable = funcoes.carregar_dados
+    )
+    #Ordem de Dependência
+    tarefa_extracao >> tarefa_carregamento
+
+
