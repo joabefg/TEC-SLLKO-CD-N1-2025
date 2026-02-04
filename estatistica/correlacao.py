c39c17ecdf6fd96e2ae4a322b1f1d075975bf551
EFG <efg@efg.local>
2026-02-04 20:56:58 -0300

    subindo pasta estatistica

---
+++ estatistica/correlacao.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# 1. Carregar os dados
+df = pd.DataFrame({
+    'alunos': ['Matheus', 'Fernando', 'Ana', 'Beatriz', 'Bruna'],
+    'estudo_h': [2, 4, 6, 8, 10],
+    'notas': [50, 70, 85, 95, 100],
+    'videogame_h': [9, 8, 5, 4, 2]
+})
+# 2. Calcular a matriz de correlação
+matriz_corr = df.corr(numeric_only=True)
+print(matriz_corr)
+# 3. Criar gráfico de dispersão
+fig, (disp, calor) = plt.subplots(1, 2, figsize=(10,5))
+sns.regplot(x = 'videogame_h', y = 'notas', data = df, truncate=False, ax=disp)
+disp.set_title('Gráfico de Dispersão')
+disp.grid(True)
+# 3.1 Criar o heatmap
+sns.heatmap(matriz_corr,
+            annot=True,
+            cmap="coolwarm",
+            fmt=".2f",
+            ax = calor)
+calor.set_title("Mapa de Calor de Correlação")
+# 4. Exibir Gráfico
+plt.show()
+# https://codeshare.io/aymZnz
---
+++ estatistica/desvios_casos_uso.py
@@ -0,0 +1,66 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# 1. Importar dados
+df = pd.read_csv('estatistica/funcionarios.csv')
+
+# 2. Definir o gráfico
+plt.figure(figsize=(22,6))
+
+# 3. Cálculos e Subplot
+analises = {}
+# 3.1 Amplitude Salarial
+analises['1. Amplitude Salarial'] = df['Salario'].max() - df['Salario'].min()
+media_salarios = df['Salario'].mean()
+desvio_salarios = df['Salario'].std()
+plt.subplot(1, 5, 1)
+plt.title('Salários')
+sns.stripplot(x=df['Salario'], jitter=True, color='skyblue')
+plt.axvline(media_salarios, color='red')
+plt.axvline(media_salarios - desvio_salarios, color='gray')
+# 3.2 Desvio Padrão Performance
+analises['2. Desvio Padrão Performance'] = df['Nota_Performance'].std()
+media_performance = df['Nota_Performance'].mean()
+desvio_performance = df['Nota_Performance'].std()
+plt.subplot(1, 5, 2)
+plt.title('Performance')
+sns.stripplot(x=df['Nota_Performance'], jitter=True, color='olive')
+plt.axvline(media_performance, color='red')
+plt.axvline(media_performance - desvio_performance, color='gray')
+# 3.3 DAM Engajamento
+analises['3. DAM Engajamento'] = (df['Engajamento'] - df['Engajamento'].mean()).abs().mean()
+media_engajamento = df['Engajamento'].mean()
+desvio_engajamento = df['Engajamento'].std()
+plt.subplot(1, 5, 3)
+plt.title('Clima Organizacional')
+sns.stripplot(x=df['Engajamento'], jitter=True, color='gold')
+plt.axvline(media_engajamento, color='red')
+plt.axvline(media_engajamento - desvio_engajamento, color='gray')
+# 3.4 Variância Meses p/ promoção
+analises['4. Variância das promoções'] = df['Meses_ate_Promocao'].var()
+media_promocao = df['Meses_ate_Promocao'].mean()
+desvio_promocao = df['Meses_ate_Promocao'].std()
+plt.subplot(1, 5, 4)
+plt.title('Meritocracia Promoções')
+sns.stripplot(x=df['Meses_ate_Promocao'], jitter=True, color='teal')
+plt.axvline(media_promocao, color='red')
+plt.axvline(media_promocao - desvio_promocao, color='gray')
+# 3.5 Amplitude Custo de Saúde
+analises['3.5 Amplitude Custo de Saúde'] = df['Custo_Saude_Anual'].var()
+media_saude = df['Custo_Saude_Anual'].mean()
+desvio_saude = df['Custo_Saude_Anual'].std()
+plt.subplot(1, 5, 5)
+plt.title('Risco Atuarial')
+sns.stripplot(x=df['Custo_Saude_Anual'], jitter=True, color='orchid')
+plt.axvline(media_saude, color='red')
+plt.axvline(media_saude - desvio_saude, color='gray')
+
+# 4. Imprimir os valores
+for caso, valor in analises.items():
+    print(f"{caso}: {valor:.2f}")
+
+# 5. Exibir o gráfico
+plt.tight_layout(rect=[0, 0.3, 1, 0.95])
+plt.show()
