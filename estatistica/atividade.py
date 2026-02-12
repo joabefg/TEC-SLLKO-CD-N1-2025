5a86c1d0eeeb39772b64608cf4fb89ce89c78685
EFG <efg@efg.local>
2026-02-11 21:01:22 -0300

    regressao

---
+++ estatistica/atividade.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
+
+dados = {
+    'Maquina_A': [28, 30, 31, 29, 30, 32, 27, 30, 31, 32],
+    'Maquina_B': [30, 20, 40, 30, 35, 25, 30, 15, 45, 30]
+}
+df = pd.DataFrame(dados)
+
+# Maquina A
+media_A =df['Maquina_A'].mean()
+mediana_A = df['Maquina_A'].median()
+variancia_A = df['Maquina_A'].var()
+desvio_padrao_A = df['Maquina_A'].std()
+cv_A = (desvio_padrao_A / media_A) * 100
+print(f"Maquina A: Média: {media_A} Mediana: {mediana_A} Variância: {variancia_A:.2f} Desvio Padrão: {desvio_padrao_A:.2f} coef. var: {cv_A}")
+
+# Máquina B
+media_B =df['Maquina_B'].mean()
+mediana_B = df['Maquina_B'].median()
+variancia_B = df['Maquina_B'].var()
+desvio_padrao_B = df['Maquina_B'].std()
+cv_B = (desvio_padrao_B / media_B) * 100
+print(f"Maquina b: Média: {media_B} Mediana: {mediana_B} Variância: {variancia_B:.2f} Desvio PAdrão: {desvio_padrao_B:.2f} coef. var: {cv_B}")
+
+# Gráfico
+fig, (dispersao, heatmap) = plt.subplots(1, 2, figsize=(10, 5))
+df.plot.scatter(x='Maquina_A', y='Maquina_B', ax = dispersao)
+heatmap = sns.heatmap(df.corr(), vmin=-1, vmax=1, ax=heatmap)
+
+plt.show()
---
+++ estatistica/regressao_alunos.py
@@ -0,0 +1,32 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.DataFrame({
+    'alunos': ['Mateus', 'Fernando', 'Ana', 'Beatriz', 'Bruna'],
+    'estudo_h': [2, 4, 6, 8, 10],
+    'notas': [50, 70, 85, 95, 100],
+    'videogame_h': [9, 8, 5, 4, 2]
+})
+
+predictors = ['estudo_h']
+outcome = 'notas'
+
+model = LinearRegression()
+model.fit(df[predictors], df[outcome])
+
+intercepto = model.intercept_
+coef_regressao = model.coef_[0]
+
+print(f'Intercepto: {intercepto}')
+print(f'Coeficiente de Regressão: {coef_regressao}')
+
+# 1. Pedir a nota do aluno
+print("***SIMULADOR DE NOTA PREVISTA***")
+entrada = input("Digite a quantidade de horas estudadas: ")
+horas_estudo = float(entrada)
+
+# 2. Calcular a nota usando a Equação de Regressão ( y = A + Bx)
+nota_prevista = intercepto + (coef_regressao * horas_estudo)
+
+# 3. Exibir o resultado formatado
+print(f'Com {horas_estudo} horas de estudo, sua nota prevista é: {nota_prevista:.2f}')
