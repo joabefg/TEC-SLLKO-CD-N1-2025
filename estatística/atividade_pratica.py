d1220316f8f71d4e1a90b09139d0fc19008b60db
EFG <efg@efg.local>
2026-02-20 21:02:31 -0300

    finalizando estatísticas

---
+++ estatística/atividade_pratica.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+dados ={
+'Maquina_A':[28, 30, 31, 29, 30, 32, 27, 30, 31, 32],
+'Maquina_B':[30, 20, 40, 30, 35, 25, 30, 15, 45, 30]
+}
+df = pd.DataFrame(dados)
+
+# Maquuina A
+media_A = df['Maquina_A'].mean()
+mediana_A = df['Maquina_A'].median()
+variancia_A = df['Maquina_A'].var()
+desvio_padrao_A = df['Maquina_A'].std()
+cv_A = (desvio_padrao_A / media_A) * 100
+print(f"Maquina A: {media_A} Mediana: {mediana_A} Variancia: {variancia_A:.2f} desvio_padrao_A: {desvio_padrao_A:.2f} coef. var.: {cv_A}")
+
+
+# Maquuina B
+media_B = df['Maquina_B'].mean()
+mediana_B = df['Maquina_B'].median()
+variancia_B = df['Maquina_B'].var()
+desvio_padrao_B = df['Maquina_B'].std()
+cv_B = (desvio_padrao_B / media_B) * 100
+print(f"Maquina B: {media_B} Mediana: {mediana_B} Variancia: {variancia_B:.2f} desvio_padrao_B: {desvio_padrao_B:.2f} coef. var.: {cv_B}")
+
+fig, (dispersao, heatmap) = plt.subplots(1, 2, figsize=(10, 5))
+df.plot.scatter(x='Maquina_A', y='Maquina_B', ax= dispersao)
+heatmap = sns.heatmap(df.corr(), vmin=-1, vmax=1, ax=heatmap)
+
+plt.show()
+
---
+++ estatística/regressao_notas.py
@@ -0,0 +1,38 @@
+import numpy as np
+import matplotlib.pyplot as plt 
+import seaborn as sns
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import mean_squared_error, r2_score
+
+# 1. Dados
+x = np.array([[2], [4], [6], [8], [10]])
+y = np.array([50, 70, 85, 95, 100])
+
+# 2. Treinando o modelo
+modelo = LinearRegression()
+modelo.fit(x, y)
+
+# 3. Fazendo as previsões
+y_pred = modelo.predict(x)
+
+# 4. Calculando Métricas de Validação
+mse = mean_squared_error(y, y_pred)
+rmse = np.sqrt(mse)
+r2 = r2_score(y, y_pred)
+
+print(f"RMSE: A média de erro da nota é: {rmse:.2f}")
+print(f"R² Scorre: {r2:.4f}")
+
+# 5. Cáulculo dos resíduos
+residuos = y - y_pred
+
+# Gráfico dos Resíduos
+plt.figure(figsize=(12,5))
+plt.subplot(1, 2, 1)
+plt.axhline(y=0, color='r', linestyle='--')
+sns.scatterplot(x=y_pred, y=residuos)
+plt.title("Gráfico de redíduos")
+plt.xlabel("Valores previstos")
+plt.ylabel("Resíduos")
+plt.show()
+
