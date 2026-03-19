423779786dba547443cd388b3bd6ea433c9077ff
joabefg <joabmartinsefg@outlook.com>
2026-03-18 21:16:50 -0300

    limpeza

---
+++ ferramentas de analise/limpeza.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+
+# 1. Importar o arquivo csv
+df = pd.read_csv('ferramentas de analise/dados.csv')
+print(df)
+
+# 2. Remover duplicatas
+df = df.drop_duplicates(subset=['ID_Pedido'], keep='first')
+print(df)
+
+# 3. Captular produtos
+df['Produto'] = df['Produto'].str.strip().str.capitalize()
+print(df)
+
+# 4. Substituir incorretos
+df['Produto'] = df['Produto'].replace('Noteboke', 'Notebook')
+print(df)
